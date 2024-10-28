2088e9b9a687b04bbd50f40d1eb451182145dbf5
sumit-dhanawade-deltatre <118807163+sumit-dhanawade-deltatre@users.noreply.github.com>
2024-10-28 13:29:02 +0530

    Correction

--- MyFirstTest.js
+++ MyFirstTest.js
@@ -2,6 +2,8 @@ const suitest = require('suitest-js-api');
 
 async function myTest() {
     await suitest.openApp();
+    suitest.sleep(20000);
+
 }
 
 myTest().finally(() => suitest.closeSession());
