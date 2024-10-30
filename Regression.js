5c13ed34562aba62275e9ec3ef12a3fcbc07070c
sumit-dhanawade-deltatre <118807163+sumit-dhanawade-deltatre@users.noreply.github.com>
2024-10-30 11:19:35 +0530

    update

--- Regression.js
+++ Regression.js
@@ -1,6 +1,6 @@
 const suitest = require('suitest-js-api');
-const { verifyCHD1Row } = require('./CHD1Rows');
-const { verifyEPG1Row } = require('./EPG1Rows');
+const { verifyCHD1Row } = require('/TestCases/RowTypes/LinearRows/CHD1Rows');
+const { verifyEPG1Row } = require('/TestCases/RowTypes/LinearRows/EPG1Rows');
 
 async function myTest() {
     await verifyCHD1Row();
